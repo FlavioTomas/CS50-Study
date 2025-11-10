11e3d492ef4d844e326ac82f9cf5c332ba698e97
Flavio <flavio.tomas.villa@gmail.com>
2025-11-09 23:39:08 -0300

    Usando long para numeros maiores

--- calculator.c
+++ calculator.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main(void){
-    int dollars = 1;
+    long dollars = 1;
     while(true){
-        char c = get_char("Here's $%i. Double it and give it to the next person? ", dollars);
+        char c = get_char("Here's $%li. Double it and give it to the next person? ", dollars);
         if (c == 'y' || c == 'Y'){
             dollars *= 2;
         }else{
             break;
         }
     }
-    printf("Here's $%i", dollars);
+    printf("Here's $%li", dollars);
 }
