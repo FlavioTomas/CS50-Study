9a7f255789fdd8b582755de73343647b45a03d83
Flavio <flavio.tomas.villa@gmail.com>
2025-11-09 23:16:32 -0300

    Criando a função print_block que imprime um bloco do tamanho que o usuario escolher

--- mario.c
+++ mario.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
-
+void print_block(void);
 
 int main(void){
-    for (int row = 0; row < 3; row++){
-        for (int col = 0; col < 3; col++){
+    print_block();
+}
+
+void print_block(void){
+    int r, c;
+    do{
+        printf("What's the number of rows of the block?\n");
+        scanf("%i", &r);
+        printf("What's the number of columns of the block?\n");
+        scanf("%i", &c);
+    }while(r < 1 && c < 1);
+
+
+    for (int i = 0; i < r; i++){
+        for (int j = 0; j < c; j++){
             printf("#");
-        }   
+        }
         printf("\n");
-    }
+    }   
 }
