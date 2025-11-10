3154568ce1abafb4b2bec867c82991ad24b48cca
Flavio <flavio.tomas.villa@gmail.com>
2025-11-09 23:49:38 -0300

    Fazendo divisão entre 2 numeros

--- calculator.c
+++ calculator.c
@@ -2,14 +2,8 @@
 #include <stdio.h>
 
 int main(void){
-    long dollars = 1;
-    while(true){
-        char c = get_char("Here's $%li. Double it and give it to the next person? ", dollars);
-        if (c == 'y' || c == 'Y'){
-            dollars *= 2;
-        }else{
-            break;
-        }
-    }
-    printf("Here's $%li", dollars);
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+
+    printf ("%.2f", (float) x/y);
 }
