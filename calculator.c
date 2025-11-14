f8a008a0da25735f69e0cc5b2c4e8cb9c201ecf9
Flavio <flavio.tomas.villa@gmail.com>
2025-11-13 22:03:38 -0300

    Criando função de soma e multiplicação na calculadora

--- calculator.c
+++ calculator.c
@@ -1,9 +1,38 @@
 #include "cs50.c"
 #include <stdio.h>
 
-int main(void){
-    int x = get_int("x: ");
-    int y = get_int("y: ");
+void soma(void);
+void mult(void);
 
-    printf ("%.2f", (float) x/y);
+
+int main(void){
+    soma();
+}
+
+
+
+
+
+
+
+void soma(void){
+    printf("\tDigite os numeros que quer somar, digite um numero <= 0 se ja digitou todos os numeros que deseja somar!\n");
+    float n = 1, soma;
+    do{
+        printf("Que numero deseja adicionar a soma? ");
+        scanf("%f", &n);
+        soma += n;
+    }while (n > 0);
+    printf("Soma dos numeros: %f", soma);
+}
+
+void mult(void){
+    printf("\tDigite os numeros que quer multiplicar, digite 0 se ja digitou todos os numeros que deseja multiplicar!\n");
+    float n = 1, mult = 1;
+    do{
+        printf("Que numero deseja adicionar a multiplicaçao? ");
+        scanf("%f", &n);
+        mult *= n;
+    }while (n > 0);
+    printf("Multiplicaçao dos numeros: %f", mult);
 }
