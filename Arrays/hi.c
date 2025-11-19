3ca1507e0c28dfffe6195f9f34652486ef973453
FlavioTomas <flavio.tomas.villa@gmail.com>
2025-11-19 16:30:48 -0300

    Printando uma string como uma array

--- Arrays/hi.c
+++ Arrays/hi.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void){
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
-    printf("%i %i %i\n", c1, c2, c3);
+    string s = "HI!";
+    printf("%c%c%c\n", s[0], s[1], s[2]);
 }
