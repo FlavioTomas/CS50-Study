ce8dee9c8295bd1364f7812d5fa47313344d96fa
FlavioTomas <flavio.tomas.villa@gmail.com>
2025-11-19 16:45:59 -0300

    Printando strings como uma array, dentro de uma array de strings

--- Arrays/hi.c
+++ Arrays/hi.c
@@ -2,6 +2,10 @@
 #include <cs50.h>
 
 int main(void){
-    string s = "HI!";
-    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
+    string words[2];
+    words[0] = "HI!";
+    words[1] = "BYE!";
+
+    printf("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
+    printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
 }
