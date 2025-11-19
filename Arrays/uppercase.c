aeb40989ff3075c3f99eac7d23f7ae5a8be012a4
FlavioTomas <flavio.tomas.villa@gmail.com>
2025-11-19 17:25:48 -0300

    usando toupper da biblioteca ctype.h

--- Arrays/uppercase.c
+++ Arrays/uppercase.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -7,14 +8,7 @@ int main(void){
     string s = get_string("Before: ");
     printf("After:  ");
     for (int i = 0, l = strlen(s); i < l; i++){
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            // change s[i] to uppercase
-            printf("%c", s[i] - ('a' - 'A'));
-        }else
-        {
-            printf("%c", s[i]);
-        }
+        printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
